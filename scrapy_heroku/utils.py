c9c03a87f4e0bb2b96916412a27fefc1b448cb49
c9dong <davidCdong_123@hotmail.com>
2018-12-26 23:24:18 -0500

    fix var

--- scrapy_heroku/utils.py
+++ scrapy_heroku/utils.py
@@ -4,8 +4,8 @@ from queue import PgQueue
 
 def get_spider_queues(config):
   """Return a dict of Spider Queues keyed by project name"""
-  d = {}
+  queues = {}
   for project in get_project_list(config):
     table = 'scrapy_%s_queue' % project
     queues[project] = PgQueue(config, table=table)
-  return d
+  return queues
